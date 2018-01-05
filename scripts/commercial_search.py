88987ea7352edf4ec9bb88e8147d59077f89c6a0
Haotian <haotianz@stanford.edu>
2018-01-05 22:33:18 +0000

    add multithread for computing histogram

--- scripts/commercial_search.py
+++ scripts/commercial_search.py
@@ -372,7 +372,7 @@ def solve_single_video(video_name):
 def test_single_video(video_name):
     commercial_gt = load_commercial_groundtruth()
     video_desp, commercial_list, raw_commercial_list, lowertext_window_list, blanktext_window_list = solve_single_video(video_name)
-    if video_desp is None
+    if video_desp is None:
         return 
     
     if video_name in commercial_gt:
@@ -416,10 +416,11 @@ def test_video_list_unsupervised(video_list_path):
         print(video_name)
         result[video_name] = {}
         video_desp, commercial_list, raw_commercial_list, lowertext_window_list, blanktext_window_list = solve_single_video(video_name)
-        result[video_name]['commercial'] = commercial_list
+        if video_desp is None:
+            continue
 
+        result[video_name]['commercial'] = commercial_list
         visualize_commercial(commercial_list, video_desp, None, raw_commercial_list, lowertext_window_list, blanktext_window_list)
-        
         # unsupervised test
         commercial_length = 0
         for com in commercial_list:
@@ -443,18 +444,22 @@ def prepare_black_frame_list(video_list_path):
             black_frame_dict[video_name] = black_frame_list
             pickle.dump(black_frame_dict, open("../data/black_frame_dict.p", "wb" ))
 
-def get_black_frame_list_t(video_list, black_frame_dict_path):
+def get_black_frame_list_t(video_list, black_frame_dict_path, thread_id):
+    print("Thread %d start computing..." % (thread_id))
     black_frame_dict = {}
     i = 0
     for video_name in video_list:
         if len(video_name) < 5:
             continue
-        print(i, video_name)
+        print("Thread %d start %dth video..." % (thread_id, i))
+        print(video_name)
         video_path = '../data/videos/' + video_name + '.mp4'
         black_frame_list = get_black_frame_list(video_path)
         black_frame_dict[video_name] = black_frame_list
         pickle.dump(black_frame_dict, open(black_frame_dict_path, "wb" ))
         i += 1
+    print("Thread %d finished computing" % (thread_id))
+
         
 def prepare_black_frame_list_multithread(video_list_path, nthread=4):
     video_list = open(video_list_path).read().split('\n')
@@ -482,7 +487,7 @@ def prepare_black_frame_list_multithread(video_list_path, nthread=4):
             video_list_t = video_list[i*num_video_t : (i+1)*num_video_t]
         else:
             video_list_t = video_list[i*num_video_t : ]
-        t = threading.Thread(target=get_black_frame_list_t, args=(video_list_t, black_frame_dict_list[i],))
+        t = threading.Thread(target=get_black_frame_list_t, args=(video_list_t, black_frame_dict_list[i], i,))
         t.setDaemon(True)
         thread_list.append(t)
     
