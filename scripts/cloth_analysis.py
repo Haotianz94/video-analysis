6922dc90c90aa74c2cab390027c99ad229dbff92
Haotian <haotianz@stanford.edu>
2018-03-23 01:22:14 +0000

    Add clothing scripts

--- scripts/cloth_analysis.py
+++ scripts/cloth_analysis.py
@@ -2,8 +2,11 @@ import colorsys
 import copy
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 import os
 import random
+import pickle
+from datetime import datetime
 from utility import *
 
 
@@ -11,7 +14,7 @@ attri_name = ['pattern', 'major_color', 'necktie', 'collar', 'scarf', 'sleeve', 
               'glass', 'layer', 'necktie_color', 'necktie_pattern', 'hair_color', 'hair_length', 'hair_colorNN']
 attri_dict = [
         {'solid': 0, 'graphics' : 1, 'striped' : 2, 'floral' : 3, 'plaid' : 4, 'spotted' : 5}, # clothing_pattern
-        {'black' : 0, 'white' : 1, 'more color' : 2, 'blue' : 3, 'gray' : 4, 'red' : 5,
+        {'black' : 0, 'white' : 1, '2+ color' : 2, 'blue' : 3, 'gray' : 4, 'red' : 5,
                 'pink' : 6, 'green' : 7, 'yellow' : 8, 'brown' : 9, 'purple' : 10, 'orange' : 11,
                 'cyan' : 12, 'dark blue' : 13}, # major_color
         {'necktie no': 0, 'necktie yes' : 1}, # wearing_necktie
@@ -24,14 +27,16 @@ attri_dict = [
         {'jacket no': 0, 'jacket yes' : 1}, # wearing_jacket
         {'hat no': 0, 'hat yes' : 1}, # wearing_hat
         {'glasses no': 0, 'glasses yes' : 1}, # wearing_glasses
-        {'one layer': 0, 'more layer' : 1}, # multiple_layers
-        {'black' : 0, 'white' : 1, 'more color' : 2, 'blue' : 3, 'gray' : 4, 'red' : 5,
-                'pink' : 6, 'green' : 7, 'yellow' : 8, 'brown' : 9, 'purple' : 10, 'orange' : 11,
-                'cyan' : 12, 'dark blue' : 13}, # necktie_color
-        {'solid' : 0, 'striped' : 1, 'spotted' : 2}, # necktie_pattern
-        {'black' : 0, 'white': 1, 'blond' : 2, 'brown' : 3, 'gray' : 4}, # hair_color
-        {'long' : 0, 'medium' : 1, 'short' : 2, 'bald' : 3}, # hair_longth
-        {'black' : 0, 'white': 1, 'blond' : 2, 'brown' : 3, 'gray' : 4} # hair_color
+        {'one layer': 0, '2+ layer' : 1}, # multiple_layers
+        {'black necktie' : 0, 'white necktie' : 1, '2+ color necktie' : 2, 'blue necktie' : 3, 'gray necktie' : 4, 'red necktie' : 5,
+            'pink necktie' : 6, 'green necktie' : 7, 'yellow necktie' : 8, 'brown necktie' : 9, 'purple necktie' : 10, 'orange necktie' : 11, 'cyan necktie' : 12, 'dark blue necktie' : 13}, # necktie_color
+#         {'black' : 0, 'white' : 1, '2+ color' : 2, 'blue' : 3, 'gray' : 4, 'red' : 5,
+#                 'pink' : 6, 'green' : 7, 'yellow' : 8, 'brown' : 9, 'purple' : 10, 'orange' : 11,
+#                 'cyan' : 12, 'dark blue' : 13}, # necktie_color
+        {'solid necktie' : 0, 'striped necktie' : 1, 'spotted necktie' : 2}, # necktie_pattern
+        {'black hair' : 0, 'white hair': 1, 'blond hair' : 2, 'brown hair' : 3, 'gray hair' : 4}, # hair_color
+        {'long hair' : 0, 'medium hair' : 1, 'short hair' : 2, 'bald' : 3}, # hair_longth
+        {'black hair' : 0, 'white hair': 1, 'blond hair' : 2, 'brown hair' : 3, 'gray hair' : 4} # hair_color
 ]
 attri_num = len(attri_name)
 attri_dict_inv = []
@@ -84,7 +89,7 @@ def sort_images_by_NN(images):
         A[i] = np.linalg.norm(dist, axis=1)
 
     # Nearest neighbour algorithm
-    path, _ = NN(A, 1000)
+    path, _ = NN(A, 0)
 
     # Final array
     images_sort = []
@@ -131,3 +136,56 @@ def sort_images_by_step(images):
     for idx in images_idx:
         images_sort.append(images[idx])
     return images_sort
+
+def multi_plot(images, num_col, size=(16, 16), titles=None, output=None):
+    fig = plt.figure()
+    fig.set_size_inches(size[0], size[1])
+    num_images = len(images)
+    num_row = int(np.ceil(1. * num_images / num_col))
+    for idx, img in enumerate(images):
+        if img is None:
+            continue
+        plt.subplot(num_row, num_col, idx+1)
+        plt.imshow(cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_BGR2RGB))
+        if titles is None:
+            plt.title(str(idx+1))
+        else:
+            plt.title(titles[idx])
+        plt.gca().axes.get_yaxis().set_visible(False)
+        plt.gca().axes.get_xaxis().set_visible(False)
+    if not output is None:
+        plt.savefig(output)
+    plt.show()
+    
+def single_plot(image, size=(16, 16), title=None, output=None):
+    fig = plt.figure()
+    fig.set_size_inches(size[0], size[1])
+    plt.imshow(cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2RGB)) 
+    if not title is None:
+        plt.title(title)
+    plt.gca().axes.get_yaxis().set_visible(False)
+    plt.gca().axes.get_xaxis().set_visible(False)
+    if not output is None:
+        plt.savefig(output)
+    plt.show()
+    
+def draw_bbox(image, bbox, use_ratio=False):
+    if use_ratio:
+        H, W, C = image.shape
+        x1 = int(bbox['bbox_x1'] * W)
+        y1 = int(bbox['bbox_y1'] * H)
+        x2 = int(bbox['bbox_x2'] * W)
+        y2 = int(bbox['bbox_y2'] * H)
+    else:
+        x1 = bbox['bbox_x1']
+        y1 = bbox['bbox_y1']
+        x2 = bbox['bbox_x2']
+        y2 = bbox['bbox_y2']
+    cv2.rectangle(image, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=3)
+    return image
+
+def get_week_bin(date):
+    dayofweek = datetime(date[0], date[1], date[2]).weekday()
+    if date[3] < 5:
+        dayofweek = (dayofweek - 1) % 7
+    return dayofweek
